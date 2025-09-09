77b0a4b56873e0280066dcb3953bc46d2783d3c9
Pedro Henrique Venâncio <160691306+phvenancio@users.noreply.github.com>
2025-09-09 20:20:08 -0300

    Delete ldw-python-flask directory

--- ldw-python-flask/ex/exercicio_01/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-from controllers import routes
-
-app = Flask(__name__, template_folder="views")
-routes.init_app(app)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4000, debug=True) 
-    
